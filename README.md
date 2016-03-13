f3db15f92c03d19762151f3d831e7dd4bd26260b
Jose Pereira <onaips@gmail.com>
2016-03-12 17:50:15 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# npm-install-changed
+
+Speed up your CI node builds by only running `npm install` when dependencies changed.
+
+[![version](https://img.shields.io/npm/v/npm-install-changed.svg)](http://npm.im/npm-install-changed)
+[![downloads](https://img.shields.io/npm/dm/npm-install-changed.svg)](http://npm-stat.com/charts.html?package=npm-install-changed)
+[![MIT License](https://img.shields.io/npm/l/npm-install-changed.svg)](http://opensource.org/licenses/MIT)
+
+- - -
+
+### Installation
+
+```shell
+$ npm install -g npm-install-changed
+```
+
+### Usage
+
+In the root folder of your node project run:
+
+```
+npm-install-changed
+```
+
+instead of the usual `npm install`
+
+### License
+
+> MIT License 2016 © Jose Pereira
---
+++ index.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+/**
+ * Jose Pereira <onaips@gmail.com>
+ *
+ * Run this script ensures 'npm install' is only run if there are dependency
+ *  changes between installations. It makes a huge difference in build times
+ *  on build servers.
+ */
+'use strict';
+
+var fs = require('fs');
+var spawn = require('child_process').spawn;
+var crypto = require('crypto');
+var assert = require('assert');
+
+var hashFile = '.npm-install-changed.json';
+
+var config = {};
+
+try {
+    var config = JSON.parse(fs.readFileSync(hashFile));
+} catch (e) {}
+
+/**
+ * Returns checksum of passed string.
+ */
+function checksum(str, algorithm, encoding) {
+    return crypto
+        .createHash(algorithm || 'md5')
+        .update(str, 'utf8')
+        .digest(encoding || 'hex');
+}
+
+/**
+ * Returns a hash built from all package.json dependencies and devDependencies
+ *  project names and versions.
+ */
+function packageJsonDepsHash() {
+    return new Promise(function(resolve) {
+        fs.readFile('./package.json', function(err, data) {
+            assert.ifError(err);
+
+            var packageJson = JSON.parse(data);
+            var newHash = 'emptydeps';
+            ['dependencies', 'devDependencies'].forEach(
+                function(
+                    dep) {
+                    for (var key in packageJson[dep]) {
+                        newHash = checksum(newHash + key +
+                            packageJson[dep][key]);
+                    }
+                });
+
+            resolve(newHash);
+        });
+    });
+}
+
+packageJsonDepsHash().then(function(hash) {
+    //make sure that if package.json changed, npm shrinkwrap should as well
+    if (hash === config.hash) {
+        console.log('Nothing to do.');
+        return;
+    }
+
+    //looks like package.json dependencies have changed
+    console.log('Running npm install...');
+    var npmProcess = spawn('npm', ['install'], {
+        stdio: 'inherit'
+    });
+
+    npmProcess.on('close', (code) => {
+        if (code !== 0) {
+            return;
+        }
+
+        config.hash = hash;
+
+        //only save new hash if npm install was successful
+        fs.writeFile(hashFile, JSON.stringify(config));
+    });
+
+}, function(err) {
+    console.log(err);
+});
